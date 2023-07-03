e72de4f0828e26f3e51fba37e7c3ee350d1ab960
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-07-03 23:08:37 +0000

    Estructura de los componetes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
 import React from "react";
+import ToDoList from "./ToDoList";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div className="text-center">
+		<ToDoList />
+	  </div>
 	);
+  };
+  
+  export default Home;
+
+  /* Codigo de Jorge
+  import React, { useState } from "react";
+
+const TodoList = () => {
+    const [task, setTask] = useState("");
+    const [taskList, setTaskList] = useState([]);
+    const [error, setError] = useState(false)
+    const handleTask = (event) => {
+        setTask(event.target.value); 
+    } 
+    const addTask = (event) => {
+        if (event.key == "Enter") {
+
+            if (event.target.value == "") {
+                setError(true);
+                alert("Please add a task!");
+                return;
+            }
+            setTaskList([...taskList, task]);
+            setTask("");
+            setError(false);
+        }
+    }
+    return (
+
+        <div className="text-center vh-100 text-light bg-dark d-flex align-items-center justify-content-center gap-3 flex-column">
+            
+            <label htmlFor="task" className={`${error ? "text-danger fw-bold" : "light"}`}>Task List</label>
+            
+            <input className="bg-dark text-light" type="text" id="task" value={task} onKeyDown={addTask} onChange={handleTask}/>
+            <div>
+                <ul>
+                {
+                    taskList.map((task, index) => {
+                        return <li key={index}> {task} </li>
+                    })
+                }
+                </ul>
+            </div>
+        </div>
+    );
 };
 
-export default Home;
+export default TodoList;
+*/
