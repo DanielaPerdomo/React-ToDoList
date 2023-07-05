c1af4795256d8250b59168cff66d9c9737757b3f
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-07-05 23:21:38 +0000

    Interaccividad del componente

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 
 const ToDoList = () => {
+  const [newTask, setNewTask] = useState(""); //Nuevas tareas
+  const [taskList, setTaskList] = useState([]); //Lista de Tareas
+  const handlerChange = (event) => {
+    setNewTask(event.target.value)
+  }
+  const handlerTask =(event) => {
+    if (event.key === "Enter"){
+      setTaskList([...taskList, newTask])
+      setNewTask("")
+    }
+  }
+  const deleteTask =(indexTask)=>{
+    let newTasks = taskList.filter((tasks, index) => index !== indexTask)
+    setTaskList(newTasks)
+  }
+
   return (
+    <div>
+    <h1> My To Do List</h1>
+    <input type="text" value={newTask} placeholder="Añadir nueva tarea"
+    onChange={handlerChange} onKeyDown={handlerTask}
+    />
     <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+      
+        {taskList.map((task, index)=>{ 
+          return <li className="list-group-item" key={index}> {task} <span className="ms-5" onClick={() => deleteTask(index)}>x</span></li> 
+        
+        })}
+      
     </ul>
+    </div>
   );
 };
 export default ToDoList;
