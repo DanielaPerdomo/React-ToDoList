6f31825471e29310a09d02fe060c90187859d2b8
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-07-06 00:17:03 +0000

    Modificaciones en el codigo

--- src/js/index.js
+++ src/js/index.js
@@ -10,19 +10,3 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
-
-/* Codigo de Jorge
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/TodoList.jsx";
-import TodoList from "./component/TodoList.jsx";
-
-//render your react application
-ReactDOM.render(<TodoList />, document.querySelector("#app"));
-*/
