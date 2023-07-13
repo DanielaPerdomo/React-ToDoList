c9293a341475b92df7a81bfba355cbe65493e585
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-07-13 18:06:32 +0000

    Estilos y codigos de iconos

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,36 +1,48 @@
 import React, { useState } from "react";
 
 const ToDoList = () => {
-  const [newTask, setNewTask] = useState(""); //Nuevas tareas
+  const [newTask, setNewTask] = useState(""); //Nueva tarea
   const [taskList, setTaskList] = useState([]); //Lista de Tareas
   const handlerChange = (event) => {
     setNewTask(event.target.value)
   }
+  //Funcion Anade tareas
   const handlerTask =(event) => {
-    if (event.key === "Enter"){
+    if (event.key === "Enter" && newTask != ""){
       setTaskList([...taskList, newTask])
       setNewTask("")
+    }
+    else {
+      alert('No hay tarea escrita')
     }
   }
   const deleteTask =(indexTask)=>{
     let newTasks = taskList.filter((tasks, index) => index !== indexTask)
     setTaskList(newTasks)
   }
-
+  const deleteAllTask = ()=>{
+    setTaskList ([])
+  }
   return (
-    <div>
-    <h1> My To Do List</h1>
-    <input type="text" value={newTask} placeholder="Añadir nueva tarea"
-    onChange={handlerChange} onKeyDown={handlerTask}
-    />
-    <ul className="list-group">
-      
-        {taskList.map((task, index)=>{ 
-          return <li className="list-group-item" key={index}> {task} <span className="ms-5" onClick={() => deleteTask(index)}>x</span></li> 
-        
-        })}
-      
-    </ul>
+    <div className="body">
+      <div className="container">
+        <div className="tittle">
+          <h1> My To Do List...!!!</h1>
+        </div>
+        <div className="entry">
+          <input type="text" value={newTask} placeholder="Añadir nueva tarea"
+          onChange={handlerChange} onKeyDown={handlerTask}
+          />
+        </div>
+        <ul className="list-group">
+          
+            {taskList.map((task, index)=>{ 
+              return <li className="list-group-item" key={index}> {task} <i className="delete fas fa-trash" onClick={() => deleteTask(index)}></i></li> 
+            })}
+        </ul>
+      <div>{taskList.length == 0 ? 'No hay tareas': `${taskList.length} tareas pendientes`}</div>
+        <button className="button" onClick={deleteAllTask}> Borrar Lista </button>
+      </div>
     </div>
   );
 };
