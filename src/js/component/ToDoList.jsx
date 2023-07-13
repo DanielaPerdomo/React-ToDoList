920d0db25889b51a1b2213755eaf9f8b5b71f070
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-07-13 21:56:34 +0000

    Modificaciones al codigo de las tareas

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -14,14 +14,16 @@ const ToDoList = () => {
         setNewTask("")
       }
       else {
-        alert('No hay tarea escrita')
+        alert('No hay tarea escrita') //Alert de que no ha escrito nada
       }
     }
   }
+  //Eliminar Tareas
   const deleteTask =(indexTask)=>{
     let newTasks = taskList.filter((tasks, index) => index !== indexTask)
     setTaskList(newTasks)
   }
+  //Funcion del Boton Borrar Tareas
   const deleteAllTask = ()=>{
     setTaskList ([])
   }
@@ -29,7 +31,7 @@ const ToDoList = () => {
     <div className="body">
       <div className="container">
         <div className="tittle">
-          <h1> My To Do List...!!!</h1>
+          <h1> Mi Lista de Tareas...!!!</h1>
         </div>
         <div className="entry">
           <input type="text" value={newTask} placeholder="Añadir nueva tarea"
@@ -37,15 +39,30 @@ const ToDoList = () => {
           />
         </div>
         <ul className="list-group">
-          
             {taskList.map((task, index)=>{ 
               return <li className="list-group-item" key={index}> {task} <i className="delete fas fa-trash" onClick={() => deleteTask(index)}></i></li> 
             })}
         </ul>
-      <div>{taskList.length == 0 ? 'No hay tareas': `${taskList.length} tareas pendientes`}</div>
-        <button className="button" onClick={deleteAllTask}> Borrar Lista </button>
+        <div>{taskList.length == 0
+            ? "No hay tarea pendiente"
+            : taskList.length == 1
+            ? `${taskList.length} tarea pendiente`
+            : `${taskList.length} tareas pendientes`}</div>
+          <button className="button" onClick={deleteAllTask}> Borrar Lista </button>
       </div>
     </div>
   );
 };
 export default ToDoList;
+
+
+/*
+<div>
+  {taskList.length == 0 ? `${taskList.length} "No hay tarea pendiente"`
+: taskList.length == 1 ? `${taskList.length} "tarea pendiente"`
+: taskList.length > 1 "tareas pendientes"}
+</div>
+*/
+/*
+<div>{taskList.length == 0 ? 'No hay tareas': `${taskList.length} tareas pendientes`}</div>
+*/
